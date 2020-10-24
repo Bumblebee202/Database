5889cb7c2bdeb31a715d6fd148e2458434093f2d
Сидоров Юра <Odessa202@gmail.com>
2020-10-24 17:35:58 +0300

    added interfaces

---
+++ Database/IDatabaseCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public interface IDatabaseCommand
+    {
+        DbCommand Command { get; }
+        ITransaction Transaction { get; set; }
+        int CommandTimeout { get; set; }
+        string Text { get; }
+
+        Task ExecuteNonQuery();
+        Task<IDatabaseReader> ExecuteReader();
+        Task<object> ExecuteScalar();
+        IDatabaseCommand OutputParameter<T>(string name);
+        IDatabaseCommand InputParameter<T>(string name, T value);
+        IDatabaseCommand InputParameter<T>(string name, T value, int size);
+        T Value<T>(string paramName);
+    }
+}
