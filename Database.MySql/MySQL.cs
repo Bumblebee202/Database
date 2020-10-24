dd5a39743a04d128e08f54651f16a2cfab5adc79
Сидоров Юра <Odessa202@gmail.com>
2020-10-24 19:17:29 +0300

    added MySql and MySqlCommand realisation

--- Database.MySql/MySQL.cs
+++ Database.MySql/MySQL.cs
@@ -1,10 +1,120 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data;
+using System.Threading.Tasks;
 
 namespace Database.MySql
 {
-    class MySQL
+    public class MySQL : IDatabase, IDisposable
     {
+        bool _disposed;
+        string _connectionString;
+        MySqlConnection _connection;
+
+        public MySQL() => _disposed = false;
+
+        public MySQL(string connectionString)
+        {
+            _disposed = false;
+            _connectionString = connectionString;
+        }
+
+        public string DatabaseName => _connection?.Database;
+
+        public string DataSource => _connection?.DataSource;
+
+        public int ConnectionTimeout => _connection != null ? _connection.ConnectionTimeout : -1;
+
+        public string ServerVersion => _connection?.ServerVersion;
+
+        public ConnectionState State => _connection.State;
+
+        public void SetConnectionString(string connectionString)
+        {
+            Dispose(true);
+            _connectionString = connectionString;
+        }
+
+        public async Task OpenConnection()
+        {
+            _connection = new MySqlConnection(_connectionString);
+            await _connection.OpenAsync();
+        }
+
+        public void CloseConnection() => Dispose(true);
+
+        public ITransaction BeginTransaction() => new MySQLTransaction(_connection);
+
+        public Task EndTransaction(ITransaction transaction, bool rollback)
+        {
+            return Task.Run(() =>
+            {
+                if (!rollback)
+                    transaction.Commit();
+                else
+                    transaction.Rollback();
+            });
+        }
+
+        public IDatabaseCommand Procedure(string name) => new MySQLCommand(name, _connection, CommandType.StoredProcedure);
+
+        public IDatabaseCommand Procedure(string name, ITransaction transaction)
+            => new MySQLCommand(name, _connection, CommandType.StoredProcedure, transaction);
+
+        public IDatabaseCommand Query(string text) => new MySQLCommand(text, _connection, CommandType.Text);
+
+        public IDatabaseCommand Query(string text, ITransaction transaction)
+            => new MySQLCommand(text, _connection, CommandType.Text, transaction);
+
+        public async Task Execute(IDatabaseCommand command) => await command.ExecuteNonQuery();
+
+        public async Task<T> Execute<T>(IDatabaseCommand command, Func<IDatabaseCommand, T> creator)
+        {
+            await command.ExecuteNonQuery();
+
+            return creator(command);
+        }
+
+        public async Task<IEnumerable<T>> Fill<T>(IDatabaseCommand command, Func<IDatabaseReader, T> selector)
+        {
+            await command.ExecuteNonQuery();
+
+            List<T> ts = new List<T>();
+
+            IDatabaseReader reader = await command.ExecuteReader();
+
+            while (await reader.Read())
+            {
+                T t = selector(reader);
+                ts.Add(t);
+            }
+
+            reader.Close();
+
+            return ts;
+        }
+
+        protected async virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                await _connection.DisposeAsync();
+                _connection = null;
+            }
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~MySQL() => Dispose(false);
     }
 }
--- Database.MySql/MySQLCommand.cs
+++ Database.MySql/MySQLCommand.cs
@@ -1,10 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
 
 namespace Database.MySql
 {
-    class MySQLCommand
+    public class MySQLCommand : IDatabaseCommand
     {
+        readonly MySqlCommand _command;
+        ITransaction _transaction;
+
+        public MySQLCommand(string name, MySqlConnection connection, CommandType commandType)
+        {
+            _command = new MySqlCommand(name, connection)
+            {
+                CommandType = commandType
+            };
+        }
+
+        public MySQLCommand(string name, MySqlConnection connection, CommandType commandType, ITransaction transaction)
+            : this(name, connection, commandType) => Transaction = transaction;
+
+        public DbCommand Command
+        {
+            get => _command;
+        }
+
+        public ITransaction Transaction
+        {
+            get => _transaction;
+            set
+            {
+                _transaction = value;
+                _command.Transaction = _transaction.Transaction as MySqlTransaction;
+            }
+        }
+
+        public int CommandTimeout
+        {
+            get => _command.CommandTimeout;
+            set => _command.CommandTimeout = value;
+        }
+
+        public string Text
+        {
+            get => _command.CommandText;
+        }
+
+        public async Task ExecuteNonQuery() => await _command.ExecuteNonQueryAsync();
+
+        public async Task<IDatabaseReader> ExecuteReader()
+        {
+            DbDataReader reader = await _command.ExecuteReaderAsync();
+            return new MySQLReader(reader);
+        }
+
+        public async Task<object> ExecuteScalar() => await _command.ExecuteScalarAsync();
+
+        object ToDBNull<T>(T value)
+        {
+            if (value != null)
+                return value;
+            return DBNull.Value;
+        }
+
+        MySqlParameter CreateParameter<T>(string name)
+        {
+            DbType type = typeof(T).Name switch
+            {
+                "Byte" => DbType.Byte,
+                "Int16" => DbType.Int16,
+                "Int32" => DbType.Int32,
+                "Int64" => DbType.Int64,
+                "Single" => DbType.Single,
+                "Double" => DbType.Double,
+                "Decimal" => DbType.Decimal,
+                "String" => DbType.String,
+                "Boolean" => DbType.Boolean,
+                "DateTime" => DbType.DateTime,
+                _ => throw new Exception("Error type")
+            };
+
+            return new MySqlParameter
+            {
+                ParameterName = name,
+                DbType = type
+            };
+        }
+
+        public IDatabaseCommand InputParameter<T>(string name, T value)
+        {
+            MySqlParameter parameter = CreateParameter<T>(name);
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Value = ToDBNull(value);
+
+            _command.Parameters.Add(parameter);
+            return this;
+        }
+
+        public IDatabaseCommand InputParameter<T>(string name, T value, int size)
+        {
+            MySqlParameter parameter = CreateParameter<T>(name);
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Value = ToDBNull(value);
+            parameter.Size = size;
+
+            _command.Parameters.Add(parameter);
+            return this;
+        }
+
+        public IDatabaseCommand OutputParameter<T>(string name)
+        {
+            MySqlParameter parameter = CreateParameter<T>(name);
+            parameter.Direction = ParameterDirection.Output;
+
+            _command.Parameters.Add(parameter);
+            return this;
+        }
+
+        public T Value<T>(string paramName) => (T)_command.Parameters[paramName].Value;
     }
 }
